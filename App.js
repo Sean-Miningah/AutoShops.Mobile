ea31ec6beffde0a11147c57a645113b79dc13234
Sean-Miningah <miningahsean@gmail.com>
2022-10-05 22:56:07 +0300

    Home page header

--- App.js
+++ App.js
@@ -1,16 +1,43 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack"
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
-import HomeScreen from './screens/HomeScreen';
+import HomeScreen from './screens/HomeScreen'
+import ChatScreen from './screens/ChatScreen'
+import FavouriteScreen from './screens/FavouriteScreen'
+import SettingScreen from './screens/SettingScreen'
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator()
+const Stack = createStackNavigator();
 
-export default function App() {
+const MainStackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Chat" component={ChatScreen} />
+      <Stack.Screen name="Favourite" component={FavouriteScreen} />
+      <Stack.Screen name="Settings" component={SettingScreen} />
+    </Stack.Navigator>
+  )
+}
+
+
+const BottomTab = createBottomTabNavigator();
+
+const BottomTabs = () => {
+  return (
+    <BottomTab.Navigator>
+      <BottomTab.Screen name="Home" component={HomeScreen} />
+      <BottomTab.Screen name="Main-Settings" component={MainStackNavigator} />
+    </BottomTab.Navigator>
+  );
+};
+
+export default function App() { 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <BottomTabs />
     </NavigationContainer>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,55 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, ScrollView, Image, StyleSheet } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from "@react-navigation/native"
+import { Bars3Icon } from "react-native-heroicons/solid"
+
+import GlobalStyles from '../GlobalStyles'
 
 const HomeScreen = () => {
+  const navigation = useNavigation()
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown:false,
+    })
+  }, [])
   return (
-    <View className="bg-red-400">
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView style={GlobalStyles.droidSafeArea} className="bg-white pt-5">
+
+      {/* Header */}
+      <View style= {styles.container} className="flex-row mx-4 items-center">
+        <View className="flex-1">
+          <Bars3Icon size={40} color="black" /> 
+        </View>
+        
+             
+        <View className="flex-1">
+          <Text className="text-base text-teal-100"> Welcome </Text>
+          <Text className="text-xl font-bold"> Ronald </Text>
+        </View>
+
+        <View className="">
+        <Image
+          source={{
+            uri: 'https://images.unsplash.com/photo-1664960599656-7f634fcc772f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
+          }}  
+
+          className="h-10 w-10 bg-gray-300 p-4 rounded-full"
+        />
+        </View>
+
+        
+      </View>
+
+    </SafeAreaView>
   )
 }
 
 export default HomeScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  }
+})
