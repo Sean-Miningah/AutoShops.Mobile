a7f26449c401bd97a87bad06051623d05ef07945
Sean-Miningah <miningahsean@gmail.com>
2022-11-13 17:58:53 +0300

    Global State Implementation

--- App.js
+++ App.js
@@ -6,13 +6,19 @@ import BottomTabNavigator from './navigation/TabNavigator';
 
 
 export default function App() { 
-  const [isRegistered, setIsRegistered] = useState(false)
+  const [isRegistered, setIsRegistered] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loginData, setLoginData] = useState({})
+  const [technicianListings, setTechnicianListings] = useState([]);
 
 
   const userSettings = {
     isLoggedIn,
     isRegistered,
+    technicianListings,
+    loginData,
+    setTechnicianListings,
+    setLoginData,
     setIsRegistered,
     setIsLoggedIn
   }
