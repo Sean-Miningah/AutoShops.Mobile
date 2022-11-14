f33cfccb6b824c40d2821097bf61d2ac86f431c4
Sean-Miningah <miningahsean@gmail.com>
2022-11-14 05:22:56 +0300

    Specialization Component Creation

---
+++ components/TechnicianSpecialization.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const TechnicianSpecialization = ({
+    name,
+}) => {
+  return (
+    <View>
+      <Text>{name}</Text>
+    </View>
+  )
+}
+
+export default TechnicianSpecialization
