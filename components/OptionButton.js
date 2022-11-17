0118515139b58ae625147d0c1ee78581f58af504
Sean-Miningah <miningahsean@gmail.com>
2022-11-17 04:03:10 +0300

    Filter users functionality

--- components/OptionButton.js
+++ components/OptionButton.js
@@ -1,13 +1,33 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useContext } from 'react';
+
+import AppContext from "../AppContext";
+import { client } from '../services/api/AutoShopsApi';
 
 const OptionButton = ({
   id,
   title,
 }) => {
+  const { loginData, setTechnicianListings } = useContext(AppContext);
+  const handleFilter = (value) => {
+    client.defaults.headers.common["Authorization"] = 'Bearer ' + loginData.access_token
+    client 
+      .get('auto-users/technician-listing/',
+        {
+          params : {
+            specialization: value,
+          }
+        }
+      )
+      .then((response) => {
+        setTechnicianListings(response.data)
+      })
+      .catch(e => console.log(e))
+  }
   return (
     <TouchableOpacity
       className=""
+      onPress={() => handleFilter(id)}
     >
       <View
         className="bg-teal-300 mx-2 p-3 rounded-lg"
