f81c9832dc57ce467421606490d3c3e29797132a
Sean-Miningah <miningahsean@gmail.com>
2022-11-20 00:04:19 +0300

    Bookings technician cards

---
+++ components/TechnicianBookingsCard.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const TechnicianBookingsCard = () => {
+  return (
+    <View>
+      <Text>TechnicianBookingsCard</Text>
+    </View>
+  )
+}
+
+export default TechnicianBookingsCard
