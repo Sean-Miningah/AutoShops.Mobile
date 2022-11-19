18ed78afb9045976c5e1a276149f24803e8d7227
Sean-Miningah <miningahsean@gmail.com>
2022-11-20 02:07:12 +0300

    Favourite home page layout

--- screens/HomeFavourites.js
+++ screens/HomeFavourites.js
@@ -1,11 +1,60 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image, ScrollView, ActivityIndicator } from 'react-native';
+import React, { useState, useEffect, useContext } from 'react';
+import { Bars3Icon } from "react-native-heroicons/solid";
+
+import AppContext from '../AppContext';
+import { client } from '../services/api/AutoShopsApi';
+import GlobalStyles from '../GlobalStyles';
+import HomeFaveCard from '../components/HomeFaveCard';
 
 const HomeFavourites = () => {
-  return (
-    <View>
-      <Text>HomeFavourites</Text>
-    </View>
+  const { isLoggedIn, loginData } = useContext(AppContext);
+  const [favourites, setFavourites] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(()=> {
+    if (isLoggedIn !== true){
+      navigation.navigate('LoginScreen');
+    }
+  });
+
+  useEffect(() => {
+    client.defaults.headers.common["Authorization"] = 'Bearer ' + loginData.access_token
+    const getFavourites = () => {
+      client
+        .get('auto-users/favourites/')
+        .then((response) => {
+          console.log('Favouirites  ',response.data)
+          setFavourites(response.data)
+          console.log(response.data)
+        })
+        .catch(e => console.log(e))
+    };
+
+    setLoading(true);
+    getFavourites();
+    setLoading(false);
+  }, [])
+
+  return isLoading ? <ActivityIndicator size="large" color="0000ff" /> : (
+    <ScrollView className="bg-green-50 py-2">
+      {favourites?.map(favourite => (
+        <HomeFaveCard 
+          key={favourite.id.toString()}
+          id={favourite.autouser.id}
+          personal_info={favourite.autouser}
+          region={favourite.region}
+          rating={favourite.rating}
+          profile_pic={favourite.autouser.photo}
+          shop_pic={favourite.profile_picture}
+          description={favourite.shop_description}
+          shop_motto={favourite.shop_goal}
+          skill_badge={favourite.skill_badge}
+          reviews={favourite.technician_feedback}  
+          specializations={favourite.technician_specialization} 
+        />
+      ))}
+    </ScrollView>
   )
 }
 
